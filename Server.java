09f856004a514e7e1497122a0c60ecf2296e440a
Kevin Daniel Fondevila Ricardo <kevinfondevila@gmail.com>
2020-02-16 16:10:23 -0500

    Prueba hilando todo el proceso del server
    Una vez se acepta el socket se crea un hilo que sera el encargado de todo el proceso

--- Server.java
+++ Server.java
@@ -4,29 +4,20 @@ import java.util.*;
 import java.io.*;
 
 public class Server {
-	                        
+	        
+        private static LinkedList msgs;
 	public static void main(String Args[]){
             
             ServerSocket ss = null;
             Socket s = null;            
-            LinkedList msgs = new LinkedList(); 
-            String msg;
-            DataInputStream in;
+            msgs = new LinkedList(); 
+            String msg;            
 		try{
 			ss = new ServerSocket(2000,20);                           
                         System.out.println("Server running on port: "+ss.getLocalPort());                                                
                         while(true){                            
-                            s = ss.accept();              
-                            
-                            if(s.isConnected()){
-                                in = new DataInputStream(s.getInputStream());                                
-                                msg = in.readUTF();                                
-                                if(msg.charAt(0)=='?'){                                
-                                    
-                                }else{
-                                    msgs.add(msg);
-                                    System.out.println(msg+"    ."+s.getInetAddress().toString());
-                                }
+                            s = ss.accept();                                          
+                            if(s.isConnected()){                                
                                 (new Hilo(s,msgs)).start();                                
                             }
                         }
@@ -40,9 +31,9 @@ public class Server {
 }
 
 class Hilo extends Thread{        
-        private Socket s = null;
-        
+        private Socket s = null;        
         private DataOutputStream out;
+        private DataInputStream in;
         private LinkedList msgs;            
         private StringTokenizer tk;
         String msg;
@@ -55,20 +46,26 @@ class Hilo extends Thread{
     @Override
     public void run(){                
         try{            
-            
-            out = new DataOutputStream(s.getOutputStream());
-            this.msg = "";
-            
-            if(msgs.size()==0){
-                msg= "NO hay mensajes";
+            in = new DataInputStream(s.getInputStream());                                
+            msg = in.readUTF();                                
+            if(msg.charAt(0)=='?'){
+                this.msg = "";
+                if(msgs.size()==0){
+                  msg= "NO hay mensajes";
+                }
+                for(int i = 0; i < msgs.size();i++){
+                  this.msg += msgs.get(i)+"\n";    
+                }                    
+            }else{
+                msgs.add(msg);
+                System.out.println(msg+"    ."+s.getInetAddress().toString());
             }
-            for(int i = 0; i < msgs.size();i++){
-                this.msg += msgs.get(i)+"\n";    
-            }                    
+            out = new DataOutputStream(s.getOutputStream());                                    
             out.writeUTF(msg);  
             this.msg = "";
             
-            s.close();            
+            s.close();     
+            in.close();
             out.close();            
         }catch(Exception e){
             System.out.println(e.toString());                        
