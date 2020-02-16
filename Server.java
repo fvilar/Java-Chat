d04ac2c51bb7e440be0944b35e6dbe95aa19dcda
Kevin Daniel Fondevila Ricardo <kevinfondevila@gmail.com>
2020-02-15 23:51:58 -0500

    Terminada la parte de mensajeria del servidor
    Actualmente ya se pueden enviar mensajes al servidor y este los reenvia al resto que esta en el chat, aun falta pulir un par de detalles y hacer pruebas

--- Server.java
+++ Server.java
@@ -4,21 +4,30 @@ import java.util.*;
 import java.io.*;
 
 public class Server {
-	    
-            
-        
+	                        
 	public static void main(String Args[]){
+            
             ServerSocket ss = null;
-            Socket s = null;
+            Socket s = null;            
+            LinkedList msgs = new LinkedList(); 
+            String msg;
             DataInputStream in;
 		try{
-			ss = new ServerSocket(2000,20);   
-                        System.out.println("Server running on port: "+ss.getLocalPort());                        
+			ss = new ServerSocket(2000,20);                           
+                        System.out.println("Server running on port: "+ss.getLocalPort());                                                
                         while(true){                            
-                            s = ss.accept();
+                            s = ss.accept();              
+                            
                             if(s.isConnected()){
+                                in = new DataInputStream(s.getInputStream());
                                 System.out.println("msg from: "+s.getInetAddress());
-                                (new Hilo(s)).start();
+                                msg = in.readUTF();                                
+                                if(msg.charAt(0)=='?'){                                
+                                    
+                                }else{
+                                    msgs.add(msg);
+                                }
+                                (new Hilo(s,msgs)).start();                                
                             }
                         }
 
@@ -32,26 +41,38 @@ public class Server {
 
 class Hilo extends Thread{        
         private Socket s = null;
-        private DataInputStream in;
+        
         private DataOutputStream out;
-    public Hilo(Socket s){        
+        private LinkedList msgs;            
+        private StringTokenizer tk;
+        String msg;
+    public Hilo(Socket s,LinkedList msgs){        
         this.s = s;        
+        this.msgs = msgs;
+        
     }
     
     @Override
-    public void run(){        
-        while(s.isConnected()){
+    public void run(){                
         try{            
-            in = new DataInputStream(s.getInputStream());
-            System.out.println(in.readUTF());
+            
+            out = new DataOutputStream(s.getOutputStream());
+            this.msg = "";
+            
+            if(msgs.size()==0){
+                msg= "NO hay mensajes";
+            }
+            for(int i = 0; i < msgs.size();i++){
+                this.msg += msgs.get(i)+"\n";    
+            }                    
+            out.writeUTF(msg);  
+            this.msg = "";
+            
+            s.close();            
+            out.close();            
         }catch(Exception e){
-            System.out.println(e.toString());
-            try{
-                s.close();
-            }catch(Exception e2){System.out.println(e2.toString());}
-            break;
-        }             
+            System.out.println(e.toString());                        
+        }        
+        return;
     }
-    }
-
 }
